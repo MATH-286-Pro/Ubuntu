3ca6dc99679c7e8e2772342ae2f3aea2d6c87642
MATH-286-Pro <1299065753@qq.com>
2024-09-05 20:09:33 +0800

    [Version.0.0] fix wrong google download address

--- Ubuntu/Ubuntu_Software.sh
+++ Ubuntu/Ubuntu_Software.sh
@@ -2,45 +2,49 @@
 
 # 更新系统并升级所有包
 sudo apt update -y && sudo apt full-upgrade -y
+echo "System update and full upgrade completed."
 
 # 开启小键盘
 sudo apt install numlockx -y
 echo -e "\nif [ -x /usr/bin/numlockx ]; then\n  /usr/bin/numlockx on\nfi" | sudo tee -a /etc/gdm3/Init/Default
+echo "Numlockx installation and configuration completed."
 
 # 设置 GNOME 点击行为
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
-
-# 安装 Git 并进行基本配置
-sudo apt-get install git -y
-git config --global user.name "MATH-286-Dell-Ubuntu"
-git config --global user.email "jhu114514@gmail.com"
-ssh-keygen -t rsa -b 4096 -C "jhu114514@gmail.com"
+echo "GNOME click-action setting completed."
 
 # 安装 Python pip
 sudo apt install python3-pip -y
+echo "Python3-pip installation completed."
 
 # 安装 GParted
 sudo apt install gparted -y
+echo "GParted installation completed."
 
 # 安装 Stickynotes (便笺)
 sudo add-apt-repository ppa:umang/indicator-stickynotes -y
 sudo apt-get update
 sudo apt-get install -y indicator-stickynotes
+echo "Indicator Stickynotes installation completed."
 
 # 安装 flameshot (截图软件)
 sudo apt install flameshot -y
+echo "Flameshot installation completed."
 
 # 安装 at (定时自动化工具)
 sudo apt-get install at -y
 sudo systemctl start atd
 sudo systemctl enable atd
+echo "At installation and service setup completed."
 
 # 安装 Curl (终端工具)
 sudo snap install curl
-
+echo "Curl installation completed."
+Download
 # 安装 Google Chrome
-cd ~/下载  # 假设你将文件下载到了默认的下载目录中
+cd ~/Downloads/  # 假设你将文件下载到了默认的下载目录中
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install --fix-missing ./google-chrome-stable_current_amd64.deb -y
+echo "Google Chrome installation completed."
 
-echo "脚本执行完毕，请根据需要手动完成一些后续配置。"
+echo "Script execution completed. Please manually complete any additional configurations if necessary."
